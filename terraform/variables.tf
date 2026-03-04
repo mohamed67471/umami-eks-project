c77f7702d6166ac0dc2c872b3efc2a3c9fff0eca
mohamed67471 <mohamedhassan22@hotmail.co.uk>
2026-03-04 17:29:06 +0000

    Fix: Remove unused variables that caused CI to hang on input prompts

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -46,49 +46,4 @@ variable "cluster_version" {
   default = "1.29"
 }
 
-variable "cluster_role_arn" {
-  type = string
-}
 
-variable "node_role_arn" {
-  type = string
-}
-
-variable "subnet_ids" {
-  type = list(string)
-}
-
-variable "cluster_sg_ids" {
-  type = list(string)
-}
-variable "vpc_id" {
-  type        = string
-  description = "The ID of the VPC where the EKS cluster will be created"
-}
-
-variable "vpc_cidr" {
-  type        = string
-  description = "CIDR block for the VPC"
-}
-
-
-variable "alb_dns_name" {
-  description = "DNS name of the ALB"
-  type        = string
-}
-
-variable "alb_zone_id" {
-  description = "Hosted zone ID of the ALB"
-  default     = "Z26RNL4JYFTOTI"
-}
-
-variable "route53_zone_id" {
-  description = "Hosted zone ID in Route 53 for the domain"
-  type        = string
-}
-
-variable "eks_cluster_id" {
-  description = "EKS cluster ID for dependency"
-  type        = string
-  default     = "umami-cluster"
-}
